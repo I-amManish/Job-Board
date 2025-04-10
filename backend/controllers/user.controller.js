b408eca5c76451a274420a56817b779303e22f25
Manish <148551097+I-amManish@users.noreply.github.com>
2025-04-10 18:10:27 +0530

    modified:   backend/controllers/user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken'
 
 export const register = async (req, res) => {
     try {
@@ -27,8 +28,12 @@ export const register = async (req, res) => {
                 role
             })
         
+            return res.status(201).json({
+                message:"Account created successfully.",
+                success:true
+            })
     } catch (error) {
-        
+        console.log(error);
     }
 }
 
@@ -43,7 +48,7 @@ export const login = async (req, res) => {
             });
         };
 
-        const user = await User.findOne({email});
+        let user = await User.findOne({email});
         if(!user) {
             return res.status(400).json({
                 message:"Incorrect email or password.",
@@ -57,9 +62,65 @@ export const login = async (req, res) => {
                 message:"Incorrect email or password.",
                 success: false,
                 })
+            };
+            // info: role is correct or not
+            if(role!== user.role) {
+                return res.status(400).json({
+                    message:"Account doesn't exist with current role.",
+                    success:false
+                })
+            };
+
+            const tokenData = {
+                userId: user._id
+            }
+            const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {expiresIn:'1d'});
+
+            user = {
+                _id:user._id,
+                fullName:user.fullName,
+                email:user.email,
+                phoneNumber:user.phoneNumber,
+                role:user.role,
+                profile:user.profile
             }
 
+            return res.status(200).cookie("token", token, {maxAge:1*24*60*60*1000, httpsOnly:true, sameSite:'strict'}).json({
+                message:`Welcome back ${user.fullName}`,
+                success:true
+            })
+
+
+
     } catch (error) {
-        
+        console.log(error)
+    }
+}
+
+export const logout = async (req, res) => {
+    try {
+        return res.status(200).cookie("token", "", {maxAge:0}).json({
+            message:"Logged out successfully.",
+            success:true
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const updateProfile = async (req,res) => {
+    try {
+        const {fullName, email, phoneNumber, bio, skills} = req.body;
+        if(!fullName || !email || !phoneNumber || !bio || !skills) {
+            return res.status(400).json({
+                message:"Something is missing",
+                success:false
+            });
+        };
+        const skillsArray = skills.split(",");
+        const userId = req.id; // note: middleware authentication
+
+    } catch (error) {
+        console.error();
     }
 }
